2bd6439d8242fd3d815af894747b6fc10ada670a
AzureAD\SyedAunRizvi <aun.rizvi@teraception.com>
2024-02-23 02:31:37 +0500

    fix: build issues resolve

--- src/components/datepickerSticky.tsx
+++ src/components/datepickerSticky.tsx
@@ -107,14 +107,10 @@
 
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import renderIf from 'render-if'
 
 export default function ResponsiveDatePickers(props) {
   const [defDate, setdefDate] = useState("01 January 1900")
