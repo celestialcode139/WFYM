5aafd81b4c19dc24152b3443081c229e84a8f425
Ali Rizvi <alirizvi139@gmail.com>
2024-02-29 02:24:04 +0500

    Fixed: firebase chat issue

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -1,18 +1,15 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCz47hK5kWxCDW211TAi4y7jKCxLwHlubA",
-  authDomain: "wfym-c5a5e.firebaseapp.com",
-  projectId: "wfym-c5a5e",
-  storageBucket: "wfym-c5a5e.appspot.com",
-  messagingSenderId: "186297085154",
-  appId: "1:186297085154:web:1c93a130681977ae04c2bb",
+    apiKey: "AIzaSyBnWwGpLFNaFI9DiI3TaNrTlKrPgUY2Nps",
+    authDomain: "wfym-75b0a.firebaseapp.com",
+    projectId: "wfym-75b0a",
+    storageBucket: "wfym-75b0a.appspot.com",
+    messagingSenderId: "170119075298",
+    appId: "1:170119075298:web:b5a39e9cd2761db69515bb",
 };
-
 // initialize firebase
-
 firebase.initializeApp(firebaseConfig);
 // export const auth = getAuth();
 export const firestore = firebase.firestore();
